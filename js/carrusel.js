85245102f1ac17fb2ea339e0dc1f1c07ce40e2ba
Ksquiitoox <ing.alejandroguerreno@gmail.com>
2025-11-15 03:00:15 -0300

    tp 3

--- js/carrusel.js
+++ js/carrusel.js
@@ -4,15 +4,15 @@ document.addEventListener("DOMContentLoaded", () => {
   if (!productsContainer) return;
 
   const products = [
-    //LAPTOPS
+    // LAPTOPS
     { category: "Laptops", title: "Notebook Pro 15\"", price: 750000, img: "https://images.unsplash.com/photo-1517336714731-489689fd1ca8" },
     { category: "Laptops", title: "UltraBook X", price: 890000, img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0aXhzi_CKAC8UWhtrbbKTvCC3fK8n3gtjow&s" },
     { category: "Laptops", title: "Gaming Beast", price: 1200000, img: "https://media.medion.com/cms/medion/uk/Beast-X25-Laptop_WP-Bg.jpg" },
-    //SMARTPHONES
+    // SMARTPHONES
     { category: "Smartphones", title: "OnePlus 11 Concept", price: 450000, img: "https://i.blogs.es/3de3fe/op/1366_2000.webp" },
     { category: "Smartphones", title: "Unihertz Luna", price: 1200000, img: "https://i.blogs.es/a957c3/nothing/1366_2000.webp" },
     { category: "Smartphones", title: "Realme GT3", price: 690000, img: "https://i.blogs.es/5582e9/realme-gt3/1366_2000.webp" },
-    //ACCESORIOS
+    // ACCESORIOS
     { category: "Accesorios", title: "Smartwatchs Pro", price: 45000, img: "https://istarmax.com/wp-content/uploads/2024/04/Starmax-Product-Range-Summer-2024-2.webp" },
     { category: "Accesorios", title: "Kit Gamer Pc Ex-450", price: 60000, img: "https://smarttucuman.com/img/Public/1130/43425-producto-solarmax-combo-gamer-4-en-1-rgbb.jpg" },
     { category: "Accesorios", title: "Cargador Carga Rápida", price: 50000, img: "https://imagenes.elpais.com/resizer/v2/QZTUJNWHPFGR5LTVPOFJZKCBBU.png?auth=0d8390af677bfaefdf5e239f5a41c2c6cddc80473bdaf867cb7a43170c246043&width=1960" }
@@ -21,11 +21,11 @@ document.addEventListener("DOMContentLoaded", () => {
   // Mezclar productos aleatoriamente
   const shuffledProducts = products.sort(() => Math.random() - 0.5);
 
-  // Crear wrapper del carrusel
+  // Wrapper del carrusel
   const wrapper = document.createElement("div");
   wrapper.classList.add("slider-wrapper");
 
-  // Título
+  // Título del carrusel
   const title = document.createElement("h2");
   title.classList.add("category-title");
   title.textContent = "Producto recomendado";
@@ -54,7 +54,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   wrapper.appendChild(slider);
 
-  // Botones de flecha
+  // Botones de flechas
   const prevBtn = document.createElement("button");
   prevBtn.classList.add("slider-btn", "prev-btn");
   prevBtn.textContent = "◀";
@@ -77,7 +77,7 @@ document.addEventListener("DOMContentLoaded", () => {
   productsContainer.appendChild(wrapper);
 });
 
-//Cantidad de productos
+// Función de cantidad
 function changeQty(btn, delta) {
   const qtySpan = btn.parentElement.querySelector(".qty");
   let qty = parseInt(qtySpan.textContent);
