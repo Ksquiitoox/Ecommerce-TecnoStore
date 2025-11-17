065af4eee529812f0415ca9ece198282cc1ce2c3
Ksquiitoox <ing.alejandroguerreno@gmail.com>
2025-11-17 01:45:24 -0300

    asd

--- js/navbarData.js
+++ js/navbarData.js
@@ -1,14 +1,10 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const navbar = document.getElementById('navbar');
-
-    const navbarItems = [
-      { title: "Home", url: "index.html" },
-      { title: "Categorías", url: "Categorias.html" },
-      { title: "Laptops", url: "Laptops.html" },
-      { title: "Smartphones", url: "Smartphones.html" },
-      { title: "Accesorios", url: "Accesorios.html" },
-      { title: "Login", url: "Login.html" },
-      { title: "Registro", url: "Register.html" },
-      { title: "Logout", url: "#", id: "logoutBtn" }
-    ]
-});
+const navbarItems = [
+  { title: "Home", url: "index.html" },
+  { title: "Categorías", url: "Categorias.html" },
+  { title: "Laptops", url: "Laptops.html" },
+  { title: "Smartphones", url: "Smartphones.html" },
+  { title: "Accesorios", url: "Accesorios.html" },
+  { title: "Login", url: "Login.html" },
+  { title: "Registro", url: "Register.html" },
+  { title: "Logout", url: "#", id: "logoutBtn" }
+];
